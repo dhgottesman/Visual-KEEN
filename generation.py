eacd4af64e9a74c8461bea9acfc5c14e795c9582
Daniela Gottesman <gottesman3@c-002.cs.tau.ac.il>
2024-10-08 17:39:20 +0300

    qa text only

--- generation.py
+++ generation.py
@@ -74,8 +74,6 @@ def generate_qa_from_image(mp):
     
     generation_records = []
     for i, row in tqdm(df.iterrows(), desc="row"):
-        if i < 100000:
-            continue
         image_file = os.path.join("/home/morg/dataset/imdb_wiki/wiki", row["path"])
         raw_image = Image.open(image_file)
         question = row["question_for_image"]
@@ -88,6 +86,7 @@ def generate_qa_from_image(mp):
         generation_records.append({
           "s_uri": row["s_uri"],
           "subject": row["subject"],
+          "question_for_image": question,
           "generation": generated_text[0].split("ASSISTANT:")[-1]
         })
 
@@ -99,6 +98,37 @@ def generate_qa_from_image(mp):
     part = "last"
     pd.DataFrame.from_records(generation_records).to_csv(f"data/qa_generations/part_{part}.csv")
 
+def generate_qa_from_text(mp):
+    df = pd.read_csv(os.path.abspath("data/full_dataset_subjects.csv"), index_col=0)
+    questions = pd.read_csv(os.path.abspath("data/all_questions.csv"), index_col=0)
+    df = df.merge(questions, on=["s_uri", "subject"])
+    
+    generation_records = []
+    for i, row in tqdm(df.iterrows(), desc="row"):
+        question = row["question"]
+        prompt = f"USER: \n{question}\nASSISTANT:"
+        inputs = mp.processor.tokenizer(text=prompt, return_tensors='pt')
+        inputs["input_ids"] = inputs["input_ids"].cuda()
+        inputs["attention_mask"] = inputs["attention_mask"].cuda()
+
+        output = mp.model.generate(**inputs, max_new_tokens=100, do_sample=False, output_hidden_states=True, return_dict_in_generate=True)
+
+        generated_text = mp.processor.batch_decode(output.sequences, skip_special_tokens=True)
+        generation_records.append({
+          "s_uri": row["s_uri"],
+          "subject": row["subject"],
+          "question": question,
+          "generation": generated_text[0].split("ASSISTANT:")[-1]
+        })
+
+        if len(generation_records) == 5000:
+            part = i // 5000        
+            pd.DataFrame.from_records(generation_records).to_csv(f"data/qa_generations_text/part_{part}.csv")
+            generation_records = []
+
+    part = "last"
+    pd.DataFrame.from_records(generation_records).to_csv(f"data/qa_generations_text/part_{part}.csv")
+
 if __name__ == "__main__":
     # mp = setup("llava_7B")
     # mp.model.vision_tower.config.output_hidden_states = True
@@ -108,5 +138,5 @@ if __name__ == "__main__":
     # generate_bios_from_image(mp)
 
     mp = setup("llava_7B")
-    generate_qa_from_image(mp)
+    generate_qa_from_text(mp)
 
